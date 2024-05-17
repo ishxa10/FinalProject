30387e67750a9898d8a2cad5a82681ab76b2fd49
ixa <irlacayanga@s.sfusd.edu>
2024-05-17 15:28:54 -0700

    Adding Start Menu

---
+++ start_menu.gd
@@ -0,0 +1,17 @@
+extends CenterContainer
+
+@onready var start_game_button = %StartGameButton
+
+func _ready():
+	start_game_button.grab_focus()
+	
+	
+func _on_start_game_button_pressed():
+	get_tree().change_scene_to_file("res://survivors_game.tscn")
+	
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+
+	
