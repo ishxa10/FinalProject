e7b0e353efa25153c43685d1137817ff4fd7d0f9
TiffyZeng46 <tizeng@s.sfusd.edu>
2024-05-20 15:42:12 -0700

    Add BGMusic for Start Menu

--- start_menu.gd
+++ start_menu.gd
@@ -6,6 +6,11 @@ func _ready():
 	start_game_button.grab_focus()
 	
 	
+func _process(delta):
+	if $AudioStreamPlayer2D.playing == false:
+		$AudioStreamPlayer2D.play()
+	
+	
 func _on_start_game_button_pressed():
 	get_tree().change_scene_to_file("res://survivors_game.tscn")
 	
